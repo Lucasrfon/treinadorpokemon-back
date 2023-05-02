273c8df122e08c01a76db116973b85439c94891b
Lucas Fonseca <lucasrfon@gmail.com>
2023-05-02 00:10:03 -0300

    feat: creats routers index

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import "express-async-errors";
+import router from "./routers";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
 export default app;
